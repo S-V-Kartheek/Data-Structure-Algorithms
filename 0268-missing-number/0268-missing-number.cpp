07a3c36fe07f980fa821dac5591e777666a10762
Venkata Kartheek Sanka <venkatakartheeksanka@gmail.com>
2025-07-06 06:42:01 +0530

    Time: 0 ms (100%), Space: 21.9 MB (14.48%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -3,12 +3,10 @@ public:
     int missingNumber(vector<int>& nums) {
         int xor1=0,xor2=0;
         int n=nums.size();
-        for(int i=0;i<=n;i++){
-            xor1^=i;
+        for(int i=0;i<n;i++){
+            xor2^=nums[i];
+            xor1^=(i+1);
         }
-        for(auto i:nums){
-            xor2^=i;
-        }
-        return xor1^xor2;
+          return xor1^xor2;
     }
 };
