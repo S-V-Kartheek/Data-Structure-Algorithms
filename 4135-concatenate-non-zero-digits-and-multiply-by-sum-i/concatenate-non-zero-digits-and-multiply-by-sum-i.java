ee2df9099ef809c5ab93bba6c2e9e8f44f566724
Venkata Kartheek Sanka <venkatakartheeksanka@gmail.com>
2026-07-07 15:25:39 +0530

    Time: 1 ms (99.85%) | Memory: 42.6 MB (67.71%) - LeetSync

--- 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.java
+++ 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.java
@@ -1,24 +1,45 @@
+// class Solution {
+//     public long sumAndMultiply(int n) {
+//         if(n==0) return 0;
+//         StringBuilder sb=new StringBuilder();
+//         long sum=0;
+//         while(n>0){
+//             int d=n%10;
+//             if(d!=0)
+//             {
+//             sb.append(d);
+//             sum+=d;
+//             }
+//             n=n/10;
+//         }
+//         sb.reverse();
+//         int res=0;
+
+//         for(int i=0;i<sb.length();i++){
+//             int digit=sb.charAt(i)-'0';
+//             res=res*10+digit;
+//         }
+//         return res*sum;
+//     }
+// }
 class Solution {
     public long sumAndMultiply(int n) {
-        if(n==0) return 0;
-        StringBuilder sb=new StringBuilder();
-        long sum=0;
-        while(n>0){
-            int d=n%10;
-            if(d!=0)
-            {
-            sb.append(d);
-            sum+=d;
-            }
-            n=n/10;
-        }
-        sb.reverse();
-        int res=0;
+        long sum = 0;
+        long num = 0;
+        long place = 1;
 
-        for(int i=0;i<sb.length();i++){
-            int digit=sb.charAt(i)-'0';
-            res=res*10+digit;
+        while (n > 0) {
+            int digit = n % 10;
+
+            if (digit != 0) {
+                sum += digit;
+                num = digit * place + num;
+                place *= 10;
+            }
+
+            n /= 10;
         }
-        return res*sum;
+
+        return sum * num;
     }
 }
